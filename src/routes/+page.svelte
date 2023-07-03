77e852f3129df0f9f86d21de10395b42d3336a4b
Oumnya benhassou <oumnya@users.noreply.github.com>
2023-07-03 17:58:37 +0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
 			</picture>
 		</span>
 
-		to your new<br />TESTIMG Cloud app Deployed By Cloudstation tonight 66666
+		to your new<br /> Cloud app Deployed By Cloudstation tonight 
 	</h1>
 
 	<h2>
