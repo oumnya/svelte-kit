50724c03152154773f11450eee316eee51ccd6cc
Oumnya benhassou <oumnya@users.noreply.github.com>
2023-07-19 12:57:09 +0400

    check logs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 
 <section>
 	<h1>
-		<span class="welcome Zak to Cloudstation loggs">
+		<span class="welcome Zak to Cloudstation">
 			<picture>
 				<source srcset={welcome} type="image/webp" />
 				<img src={welcome_fallback} alt="Welcome" />
